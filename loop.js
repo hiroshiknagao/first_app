deb9f2fc314a297df562845ad68b4cefa27e864a
hiroshi nagao <hi_1205@hotmail.com>
2024-07-29 14:47:48 +0900

    reviewing while, do while and break statement

--- loop.js
+++ loop.js
@@ -20,3 +20,37 @@
     }
   }
  }
+// while loop
+let counterTwo = 1;
+while (counterTwo < 4) {
+  console.log(counterTwo);
+  counterTwo++
+};
+
+const cards = ['diamond', 'spade', 'heart', 'club'];
+let currentCard;
+while (currentCard !== 'spade') {
+  currentCard = cards[Math.floor(Math.random()*4)];
+
+  console.log(currentCard);
+}
+//do while statement
+let cupsOfSugarNeeded = 4;
+let cupsAdded = 0;
+
+do {
+  cupsAdded++
+  console.log(cupsAdded + ' cup was added')
+} while (cupsAdded < cupsOfSugarNeeded);
+
+console.log(cupsOfSugarNeeded)
+
+//the break keyword
+const rapperArray = ["ODB", "Jay-Z", "Biggie", "2pac"];
+for (let i = 0; i < rapperArray.length; i ++) {
+  console.log(rapperArray[i]);
+  if (rapperArray[i] === 'Biggie') {
+    break;
+  }
+}
+console.log("And if you don't know, now you know");
