404fda27d297d4d350b0e0af54b1ccc1db93f30e
hiroshi nagao <hi_1205@hotmail.com>
2024-09-27 17:25:06 +0900

    odin basic variable

--- odin.js
+++ odin.js
@@ -1,2 +1,12 @@
-console.log(23 + 97)
-console.log(4 + 6 + 9) / 77
+//console.log(23 + 97)
+//console.log(4 + 6 + 9) / 77
+let a = 10;
+console.log(a);
+console.log(9 * a);
+let b = 7 * a
+console.log(b);
+const max = 57;
+const actual = max - 13;
+const percentage = actual / max;
+console.log(percentage);
+
