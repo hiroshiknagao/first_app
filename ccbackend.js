c6ea810296fc70f69bea838c0a58c1f6e4785423
hiroshi nagao <hi_1205@hotmail.com>
2024-10-01 17:54:02 +0900

    codecademy backend review

---
+++ ccbackend.js
@@ -0,0 +1,7 @@
+let noun1 = 'car';
+let adjective = 'slow';
+let noun2 = 'people';
+let verb = 'travel';
+let noun3 = 'steaks';
+
+console.log(`The world's first ${noun1} was very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
