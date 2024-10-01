5e7feea5e2b77b3c20d1ce4c8971dd30462f04a8
hiroshi nagao <hi_1205@hotmail.com>
2024-10-01 18:59:46 +0900

    ccbackend the console module

--- ccbackend.js
+++ ccbackend.js
@@ -1,7 +1,11 @@
-let noun1 = 'car';
-let adjective = 'slow';
-let noun2 = 'people';
-let verb = 'travel';
-let noun3 = 'steaks';
+//let noun1 = 'car';
+//let adjective = 'slow';
+//let noun2 = 'people';
+//let verb = 'travel';
+//let noun3 = 'steaks';
+//
+//console.log(`The world's first ${noun1} was very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
 
-console.log(`The world's first ${noun1} was very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
+//const petsArray = ['dog', 'cat', 'bird', 'monkey'];
+//console.table(petsArray);
+console.assert(petsArray.length > 5);
