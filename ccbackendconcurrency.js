1790cfbd567fed9140834628924ad7796dc9fe87
hiroshi nagao <hi_1205@hotmail.com>
2024-10-02 11:15:44 +0900

    asynch concurrency review

---
+++ ccbackendconcurrency.js
@@ -0,0 +1,9 @@
+//console.log("i'm learning about");
+//for (let idex=0; idex < 999999; idex++) {}
+
+//console.log("the event loop");
+console.log("i'm learing about");
+setTimeout(() => { console.log("Event Loop");
+
+}, 3500);
+console.log("the")
