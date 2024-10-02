028a2a8133473d09bef2bf72bd0be909847cda5c
hiroshi nagao <hi_1205@hotmail.com>
2024-10-02 13:11:39 +0900

    the odin project function review

---
+++ odinfunction.js
@@ -0,0 +1,18 @@
+//function favortiteAnimal(animal) {
+  //return animal + " is my favorite animal."
+//}
+//console.log(favortiteAnimal('A cat'));
+function add7(number) {
+  return number + 7
+}
+console.log(add7(7));
+
+function multiply(num1, num2) {
+  return num1 * num2
+}
+console.log(multiply(3, 2));
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+console.log(capitalize("dog"))
