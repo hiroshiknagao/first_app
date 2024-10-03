3158d9d40f31b1754fb1fa2d79aa54e54a5cb626
hiroshi nagao <hi_1205@hotmail.com>
2024-10-03 18:50:36 +0900

    top problem solving fizzbuzz

---
+++ topproblemsolving.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync") ({sigint:true});
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+
+for (let i = 1; i <= answer; i++) {
+  if (i % 3 === 0) {
+    console.log("Fizz");
+  } else {console.log(i);
+  }
+}
